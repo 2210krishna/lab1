5dce4a85ee6d2511cf6d8b0c929252840e2b29f3
2210krishna <krishna28046@gmail.com>
2025-03-13 22:50:52 +0530

    vbnjk

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -2,6 +2,8 @@ package com.examly.springapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringappApplication {
@@ -9,5 +11,10 @@ public class SpringappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringappApplication.class, args);
 	}
+	@Bean
+		public RestTemplate restTemplate() {
+		return new RestTemplate();
+		}
+		}
 
-}
+
