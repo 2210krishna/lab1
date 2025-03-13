55c01c1618e35711cfad0dc4d87b8628ca2a5256
neojarvis <techadmin@iamneo.ai>
2025-03-13 16:32:52 +0000

    Auto-save-commit-13/03/2025--22:02:51

--- springapp/src/main/java/com/examly/springapp/controller/WeatherController.java
+++ springapp/src/main/java/com/examly/springapp/controller/WeatherController.java
@@ -3,9 +3,12 @@ package com.examly.springapp.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.Value;
+
 @RestController
 @RequestMapping("/weather")
 public class WeatherController {
+    @Value
 
     
 }
